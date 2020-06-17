d1a8380c48a85b9c66f42d7253b79139002eebde
Sebastian Steenssøn <sebastian@steensson.dk>
2020-06-17 21:24:20 +0200

    Added tokenize_with_indices function

--- ner_doccano_utils.py
+++ ner_doccano_utils.py
@@ -1,11 +1,13 @@
 """
-NER utilites.
+NER doccano utilites.
 
-Collection of utility functions for during NER preprocsseing.
-Especially when data comes as doccano data.
+Collection of utility functions for during NER preprocessing.
 """
 
-from typing import List, Dict
+from typing import List, Dict, Tuple
+from spacy.util import get_lang_class
+from transformers import BertTokenizer
+import numpy as np
 import json
 
 
@@ -66,3 +68,76 @@ def extract_entities(
             labels_dict[label].append(text[label_start:label_end])
 
     return labels_dict
+
+
+def tokenize_with_indices(
+    doccano_document: Dict,
+    spacy_lang_class: str = 'da',
+    bert_model: str = 'bert-base-multilingual-cased'
+) -> List[Tuple[int, int, str]]:
+    """Tokenize text and get indices for each tokenpiece.
+
+    Parameters
+    ----------
+    doccano_document : Dict
+        Must contain 'text' key
+    spacy_lang_class : str, optional
+        Two-letter language, by default 'da'
+    bert_model : str, optional
+        BERT model, by default 'bert-base-multilingual-cased'
+
+    Returns
+    -------
+    List[Tuple[int, int, str]]
+        Each tuple contains start and end index for token as well as the token
+
+    """
+    text = doccano_document['text']
+
+    # Initiate spaCy sentencizer
+    cls = get_lang_class(spacy_lang_class)
+    sentencizer = cls()
+    sentencizer.add_pipe(sentencizer.create_pipe('sentencizer'))
+
+    # Initiate BERT tokenizer
+    tokenizer = BertTokenizer.from_pretrained(bert_model)
+
+    # Sentencize text
+    sentences = sentencizer(text).sents
+
+    token_tuples = []
+
+    # For each sentence
+    for sentence_idx, sentence in enumerate(sentences):
+        words, word_to_token_mapping = [], []
+
+        # For each word in current sentence, tokenize word and store mapping
+        for word in sentence:
+            words.append(word)
+            tokenpieces = tokenizer.tokenize(word.text)
+            word_to_token_mapping.append((word, tokenpieces))
+
+        # Map labels to token
+        for word, tokens in word_to_token_mapping:
+            # Computer tokens length
+            tokens_len = [
+                len(token) - 2 if token.startswith('##') else len(token)
+                for token in tokens
+            ]
+
+            # Computer tokens indices
+            token_start_idxs = word.idx + np.cumsum([0] + tokens_len[:-1])
+
+            # Store start index, end index and token
+            token_tuples.append(list(zip(
+                token_start_idxs, token_start_idxs + tokens_len, tokens
+            )))
+
+    # Unnest
+    token_tuples_unnested = [
+        tokenpiece
+        for word in token_tuples
+        for tokenpiece in word
+    ]
+
+    return token_tuples_unnested
